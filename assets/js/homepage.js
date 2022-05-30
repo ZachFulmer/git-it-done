2db18f9d7afbc03d3da1870cdb2c38c3de73468d
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-30 17:23:49 -0500

    dynamic fetch user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,12 @@
-var getUserRepos = function()
+var getUserRepos = function(user)
 {
-    fetch("https://api.github.com/users/octocat/repos");
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("zachfulmer");
